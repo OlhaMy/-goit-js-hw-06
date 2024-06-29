173b177ede04c329e76321e37a663f497aac919c
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-06-29 14:57:23 +0200

    1 and 2 task

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,27 @@
+class Storage {
+
+    #items;
+
+    constructor (params) {
+        this.#items = params.items;
+    }
+
+    getItems(){
+        return this.#items;
+    }
+    addItem(newItem){
+        this.#items.push(newItem);
+    }
+    removeItem(itemToRemove){
+        const index = this.#items.indexOf(itemToRemove);
+        if (index !== -1) {
+          this.#items.splice(index, 1);
+        }
+    }
+}
+
+
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
