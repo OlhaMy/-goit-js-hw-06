7c1ec44b4765b190fe339000f78a184d55dd8c7c
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-06-30 11:33:43 +0200

    task-2, 3

--- js/task-2.js
+++ js/task-2.js
@@ -1,25 +1,25 @@
 class Storage {
-
     #items;
-
-    constructor (params) {
-        this.#items = params.items;
+  
+    constructor(items) {
+      this.#items = items;
     }
-
-    getItems(){
-        return this.#items;
+  
+    getItems() {
+      return this.#items;
     }
-    addItem(newItem){
-        this.#items.push(newItem);
+  
+    addItem(newItem) {
+      this.#items.push(newItem);
     }
-    removeItem(itemToRemove){
-        const index = this.#items.indexOf(itemToRemove);
-        if (index !== -1) {
-          this.#items.splice(index, 1);
-        }
+  
+    removeItem(itemToRemove) {
+      const index = this.#items.indexOf(itemToRemove);
+      if (index !== -1) {
+        this.#items.splice(index, 1);
+      }
     }
-}
-
+  }
 
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
